e661e0e081da23335a06f50574a663c1c898f2a1
Olatunji Omisakin <msknolatunji@gmail.com>
2020-10-07 19:40:05 +0100

    Moving Component to another Folder

---
+++ src/components/MyInfo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MyInfo() {
+
+  return (
+    <div>
+      <h1>My name is Olatunji Omisakin</h1>
+      <p>I am a enthusiastic programmer</p>
+      <ul>
+        <li>Jerusalem</li>
+        <li>Silicon Valley</li>
+        <li>USA White House</li>
+      </ul>
+    </div>
+  )
+
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -2,20 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function MyInfo() {
-
-  return (
-    <div>
-      <h1>My name is Olatunji Omisakin</h1>
-      <p>I am a enthusiastic programmer</p>
-      <ul>
-        <li>Jerusalem</li>
-        <li>Silicon Valley</li>
-        <li>USA White House</li>
-      </ul>
-    </div>
-  )
-
-}
+import MyInfo from "./components/MyInfo"
 
 ReactDOM.render(<MyInfo />, document.getElementById("root"))
