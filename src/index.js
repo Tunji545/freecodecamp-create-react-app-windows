d19a1084d4ff8f00aa307a5c14f23eca6b6503fd
Olatunji Omisakin <msknolatunji@gmail.com>
2020-10-08 20:02:03 +0100

    Jsx to Javascript and back.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+
+function App() {
+
+  const date = new Date( );
+  const hour = date.getHours();
+  let time;
+  if (hour < 12) {
+    time = "Morning!";
+  }
+  else if (hour > 12 && hour < 17) {
+    time = "Afternoon!";
+  }
+  else {
+    time = "Night!";
+  }
+  return (
+    <h1>Good { time } The time is { hour % 12 } o'clock.</h1>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
