1eddf153614d3acf1df0ac077e2c8b0df50b7cb0
Olatunji Omisakin <msknolatunji@gmail.com>
2020-10-09 18:41:01 +0100

    Props in React 1A

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from "react";
-import TodoItem from "./components/TodoItem";
 import "./index.css";
 
 function App() {
 
-  return (
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+  return(
+    <div className="contacts">
+      <div className="contact-card">
+        <img src={require('./Img/bible.jpg')} alt="Bible"/>
+        <h3>The Bible</h3>
+        <p>Phone: "07012345678"</p>
+        <p>Email: "bible@bible.com"</p>
+      </div>
+      <div className="contact-card">
+        <img src={require('./Img/at-icon.jpg')} alt="at-icon"/>
+        <h3>Email Logo</h3>
+        <p>Phone: "08012345678"</p>
+        <p>Email: "email@email.com"</p>
+      </div>
+      <div className="contact-card">
+        <img src={require('./Img/facebook-icon.png')} alt="Facebook"/>
+        <h3>Facebook Logo</h3>
+        <p>Phone: "08112345678"</p>
+        <p>Email: "facebook@facebook.com"</p>
+      </div>
+      <div className="contact-card">
+        <img src={require('./Img/twitter-icon.png')} alt="Twitter"/>
+        <h3>Twitter logo</h3>
+        <p>Phone: "09012345678"</p>
+        <p>Email: "twitter@twitter.com"</p>
+      </div>
     </div>
   )
 }
