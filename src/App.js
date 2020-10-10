7f5dedd728bc123e1e7bf09cdec6e82e95fcbb63
Olatunji Omisakin <msknolatunji@gmail.com>
2020-10-10 10:53:40 +0100

    Props in React Part 1c

--- src/App.js
+++ src/App.js
@@ -7,30 +7,10 @@ function App() {
 
   return(
     <div className="contacts">
-      <ContactCard 
-        imgurl="{require('./images/bible.jpg')}" alt="Bible" 
-        name="The Bible"
-        phone="07012345678"
-        email="bible@bible.com"
-      />
-      <ContactCard 
-        imgurl="{require('./images/at-icon.jpg')}" alt="Bible" 
-        name="Email Logo"
-        phone="08012345678"
-        email="email@email.com"
-      />
-      <ContactCard 
-        imgurl="{require('./images/facebook-icon.png')}" alt="Facebook"
-        name="Facebook Logo"
-        phone="08112345678"
-        email="facebook@facebook.com"
-      />
-      <ContactCard 
-        imgurl="{require('./images/twitter-icon.png')}" alt="Twitter"
-        name="Twitter logo"
-        phone="09012345678"
-        email="twitter@twitter.com"
-      />
+      <ContactCard contact={{imgurl: "{require('./images/bible.jpg')}", alt: "Bible" , name: "The Bible", phone: "07012345678", email: "bible@bible.com"}} />
+      <ContactCard contact={{imgurl: "{require('./images/at-icon.jpg')}", alt: "Email", name: "Email Logo", phone: "08012345678", email: "email@email.com"}} />
+      <ContactCard contact={{imgurl: "{require('./images/facebook-icon.png')}", alt: "Facebook", name: "Facebook logo", phone: "08112345678", email: "facebook@facebook.com"}} />
+      <ContactCard contact={{imgurl: "{require('./images/twitter-icon.png')}", alt: "Twitter", name: "Twitter Logo", phone: "0901234567", email: "twitter@twitter.com"}} />
     </div>
   )
 }
