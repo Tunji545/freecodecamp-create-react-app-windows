bb06dbe1edfb735cc3f1cda465c29aa767bf6cbd
Olatunji Omisakin <msknolatunji@gmail.com>
2020-10-10 10:39:03 +0100

    Props in React 1b

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import React from "react";
 import "./index.css";
+import ContactCard from "./ContactCard";
+
 
 function App() {
 
   return(
     <div className="contacts">
-      <div className="contact-card">
-        <img src={require('./Img/bible.jpg')} alt="Bible"/>
-        <h3>The Bible</h3>
-        <p>Phone: "07012345678"</p>
-        <p>Email: "bible@bible.com"</p>
-      </div>
-      <div className="contact-card">
-        <img src={require('./Img/at-icon.jpg')} alt="at-icon"/>
-        <h3>Email Logo</h3>
-        <p>Phone: "08012345678"</p>
-        <p>Email: "email@email.com"</p>
-      </div>
-      <div className="contact-card">
-        <img src={require('./Img/facebook-icon.png')} alt="Facebook"/>
-        <h3>Facebook Logo</h3>
-        <p>Phone: "08112345678"</p>
-        <p>Email: "facebook@facebook.com"</p>
-      </div>
-      <div className="contact-card">
-        <img src={require('./Img/twitter-icon.png')} alt="Twitter"/>
-        <h3>Twitter logo</h3>
-        <p>Phone: "09012345678"</p>
-        <p>Email: "twitter@twitter.com"</p>
-      </div>
+      <ContactCard 
+        imgurl="{require('./images/bible.jpg')}" alt="Bible" 
+        name="The Bible"
+        phone="07012345678"
+        email="bible@bible.com"
+      />
+      <ContactCard 
+        imgurl="{require('./images/at-icon.jpg')}" alt="Bible" 
+        name="Email Logo"
+        phone="08012345678"
+        email="email@email.com"
+      />
+      <ContactCard 
+        imgurl="{require('./images/facebook-icon.png')}" alt="Facebook"
+        name="Facebook Logo"
+        phone="08112345678"
+        email="facebook@facebook.com"
+      />
+      <ContactCard 
+        imgurl="{require('./images/twitter-icon.png')}" alt="Twitter"
+        name="Twitter logo"
+        phone="09012345678"
+        email="twitter@twitter.com"
+      />
     </div>
   )
 }
