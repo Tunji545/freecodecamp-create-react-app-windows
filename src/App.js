7ba4f4ba323f0b9881de226fdbe049b3c8591e37
Olatunji Omisakin <msknolatunji@gmail.com>
2020-10-11 21:34:41 +0100

    Class Based Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
-import todoData from "./components/todoData";
-import TodoItem from "./components/TodoItem";
-import "./index.css"
 
-function App() {
-  const TodoComponent = todoData.map(todo => <TodoItem key={todo.id} text={todo.text} completed={todo.completed}/>)
-  return(
-    <div className="todo-list">
-      {TodoComponent}
-    </div>
-  )
+// function App(props) {
+
+//   return (
+//     <h2>{props.whatever}</h2>
+//   )
+// }
+
+class App extends React.Component {
+  yourMethodHere() {
+
+  }
+  render() {
+
+    return (
+      <h2>{this.props.whatever}</h2>
+    )
+  }
 }
 
 export default App;
