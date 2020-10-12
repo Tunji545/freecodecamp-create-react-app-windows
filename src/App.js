bd0bf03bf38b6f2c9c040ca6e29d6d7741cc5bf0
Olatunji Omisakin <msknolatunji@gmail.com>
2020-10-12 13:37:29 +0100

    Handling Event in React

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React from "react";
-import todoData from "./components/todoData";
-import TodoItem from "./components/TodoItem";
-import "./index.css";
+
+// function App() {
+
+//   function clickMe() {
+//     console.log("You clicked me!");
+//   }
+//   return (
+//     <div>
+//       <img src={require(`./images/bible.jpg`)} alt="bible"/>
+//       <br />
+//       <br />
+//       <h3 onClick={clickMe}>Click me.</h3>
+//     </div>
+//   )
+// }
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      todoItem: todoData 
-    }
+  }
+  clickMe() {
+    console.log("You clicked me!");
+  };
+  mouseOver() {
+    console.log("mouseOver")
   }
   render() {
-    const TodoComponent = this.state.todoItem.map(todo => <TodoItem key={todo.id} todos={todo} />);
+    
     return (
-      <div className="todo-list">
-        {TodoComponent}
+      <div>
+        <img src={require(`./images/bible.jpg`)} alt="bible"/>
+        <br />
+        <br />
+        {/* <h3 onClick={() => console.log("You clicked me!")}>Click me.</h3> */}
+        <h3 onClick={this.clickMe} onMouseOver={this.mouseOver}>Click Me.</h3>
       </div>
     )
   }
