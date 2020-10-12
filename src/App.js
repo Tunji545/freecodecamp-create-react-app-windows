22890acb3e24687a6cb18554bf213ee3209be952
Olatunji Omisakin <msknolatunji@gmail.com>
2020-10-12 08:36:14 +0100

    State Component 2

--- src/App.js
+++ src/App.js
@@ -1,49 +1,28 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      answer: "Yes"
+    }
+  }
 
   render() {
-
-    return(
-
+    return (
       <div>
-        <Heading username="vschool"/>
-        <Greeting />
+        <h2>Is state really important? {this.state.answer}</h2>
+        <ChildComponent answer={this.state.answer} />
       </div>
     )
   }
 }
 
-class Heading extends React.Component {
+class ChildComponent extends React.Component {
 
   render() {
-
     return (
-      <h3>Welcome {this.props.username}!</h3>
-    )
-  }
-}
-
-class Greeting extends React.Component {
-
-  render() {
-    const date = new Date();
-    const hour = date.getHours();
-    let timeOfDay;
-    if (hour < 12) {
-      timeOfDay = "Good Morning!";
-    }
-    else if (hour > 12 && hour < 17) {
-      timeOfDay = "Good Afternoon!";
-    }
-    else {
-      timeOfDay = "Good Night!"
-    }
-
-    return (
-      <div>
-        <h2>{timeOfDay} to you sir or ma.</h2>
-      </div>
+      <p>{this.props.answer}</p>
     )
   }
 }
