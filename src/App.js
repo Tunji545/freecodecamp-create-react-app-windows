705b84970b1c64586b34e39e65b7e428b25daefd
Olatunji Omisakin <msknolatunji@gmail.com>
2020-10-15 13:30:07 +0100

    Conditional Renderinng Part 1

--- src/App.js
+++ src/App.js
@@ -1,56 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import Conditional from "./components/Conditional";
 
-// https://engineering.musefind.com/react-lifecycle-methods-how-and-when-to-use-them-2111a1b692b1
-// https://reactjs.org/blog/2018/03/29/react-v-16-3.html#component-lifecycle-changes
-
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
+    this.state = {
+      isLoading: true
+    }
   }
-
-  static getDerrivedStateFromProps(props, state) {
-    // return the new, updated state base upon props
-    // https://reactjs.org/docs/react-component.html#static-getderivedstatefromprops
-    // https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
-  }
-
-  getSnapshotBeforeUpdate() {
-    // create a backup of the current state things are.
-    // https://reactjs.org/docs/react-component.html#getsnapshotbeforeupdate
-  }
-
-  // componentWillMount() {
-
-  // }
 
   componentDidMount() {
-    // GET thedata I need to correctly display.
-  }
-
-  // componentWillReceiveProps(nextProps) {
-  //   if (nextProps.whatever !== this.props.whatever) {
-  //     // do something important here.
-  //   }
-  // }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    // return ture if you want it to update.
-    // return false if not.
-  }
-
-  // componentWillUpdate() {
-
-  // }
-
-  componentWillUnmount() {
-    // teardowm or clean up your code before your component disappear.
-    // (E.g remove event listeners.)
+    setTimeout(() => {
+      this.setState({
+        isLoading: false
+      })
+    }, 1500)
   }
 
   render() {
     return (
       <div>
-
+        {this.state.isLoading
+        ? <h1>Loading ...</h1>
+        : <Conditional />}
+        
       </div>
     )
   }
---
+++ src/components/Conditional.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Conditional(props) {
+  // Condition ? Statement is true : Statement is false
+
+  return (
+    <div>
+      <h1>Some cool stuffs about to render.</h1>
+    </div>
+  )
+  // if (props.isLoading) {
+  //   return (
+  //     <h1>Loading ...</h1>
+  //   )
+  // } else {
+  //   return (
+  //     <h1>Some cool stuffs about to render.</h1>
+  //   )
+  // }
+}
+
+export default Conditional;
